91e8a6d6e4c9b144ae21180d39bb0ebdacdeca16
robin* <robin_maki@planet.moe>
2019-11-08 11:29:20 +0900

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+require('express-async-errors');
+
+const PORT = 8080;
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+fs.readdirSync(path.join(__dirname, 'routes')).forEach((name) => {
+    let m = require(path.join(__dirname, 'routes', name));
+    app.use(m.path, m.router);
+});
+
+app.listen(PORT, (err) => {
+    if(!err) {
+        console.log(`Listening on port ${PORT}`);
+    }
+    else {
+        console.error(err.message);
+    }
+})
