ffbbb71a9af0c76a4938c56e1d30725ebc93cd3b
robin* <robin_maki@planet.moe>
2019-11-09 15:46:29 +0900

    회원가입 1페이즈 제작

--- src/routes/front.js
+++ src/routes/front.js
@@ -5,6 +5,9 @@ router.get('/', async(req, res) => {
     // 로그인 되어있는지? 체크
     res.render('login', {layout: 'plain'}); // 임시
 });
+router.get('/signup', (req, res) => {
+    res.render('signup', {layout: 'plain'});
+});
 
 module.exports = {
     router, path: '/'
