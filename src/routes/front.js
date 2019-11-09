546fd464e596a1358e8e9be037ef513c78ded2a3
robin* <robin_maki@planet.moe>
2019-11-09 23:11:17 +0900

    pjax 적용

--- src/routes/front.js
+++ src/routes/front.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+function setLayout(req, name = 'plain') {
+    return {layout: (!req.get('X-PJAX') ? name : false)};
+}
+
 router.get('/', async(req, res) => {
     // 로그인 되어있는지? 체크
-    res.render('login', {layout: 'plain'}); // 임시
+    res.render('login', setLayout(req)); // 임시
 });
 router.get('/signup', (req, res) => {
-    res.render('signup', {layout: 'plain'});
+    res.render('signup', setLayout(req));
 });
 
 module.exports = {
