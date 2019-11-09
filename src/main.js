aeb2ee628c823df89d755ee2a1d801d2eb4fb094
robin* <robin_maki@planet.moe>
2019-11-09 13:11:23 +0900

    express에 hbs view engine 적용

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 require('express-async-errors');
 
@@ -10,6 +11,8 @@ const PORT = 8080;
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.engine('handlebars', hbs());
+app.set('view engine', 'handlebars');
 
 fs.readdirSync(path.join(__dirname, 'routes')).forEach((name) => {
     let m = require(path.join(__dirname, 'routes', name));
